fbe81d51322599d82a6ed8cac36362703a570e9f
wateer <liningning@wandoujia.com>
2015-04-14 11:22:52 +0800

    fix a bug

--- puppet_node_classifer.py
+++ puppet_node_classifer.py
@@ -5,19 +5,48 @@
 import sys
 import requests
 import json
+import re
 
 import yaml
 
 
 IDS = {
+    "online_ng_external": [
+        2394,
+        2388,
+        2401,
+        2397,
+        2405 
+    ],
+    "online_ng_internal": [
+        2392,
+        2389,
+        2400,
+        2396,
+        2404
+    ],
+    "log_ng": [
+        2338
+    ],
+    "online_cdn_proxy": [
+        2169
+    ],
     "ntp_server": [
         2574
     ]
 }
 
+KEYS = {
+    "relay": "relay",
+    "vmh": "vmh_common",
+    "cobar": "cobar"
+}
 
-def get_hostnames_from_loki(node_id):
-    url = "http://api.xxx.com/server/api/servers?"\
+IGNORES = ["ignore0.nosa01"]
+
+
+def get_hostnames_from_sys(node_id):
+    url = "http://sys.hy01.internal.nosa.me/server/api/servers?"\
             "type=recursive&node_id=%s" % node_id
 
     try:
@@ -34,7 +63,7 @@ def get_classes_to_nodes():
     for classes in IDS:
         nodes = []
         for _id in IDS[classes]:
-            _ret = get_hostnames_from_loki(_id)
+            _ret = get_hostnames_from_sys(_id)
             if not _ret[0] :
                 return (False, _ret[1])
             else:
@@ -45,6 +74,24 @@ def get_classes_to_nodes():
 
 def main():
     hostname = sys.argv[1].replace(".nosa.me", "")
+
+    # 如果 hostname 在 IGNORES 里面, 啥也不做. 
+    if hostname in IGNORES:
+        print yaml.dump({})
+        return 
+
+    # 如果 hostname 匹配 KEYS 里面关键字, 返回相应的配置.
+    for i in KEYS:
+        if re.match("^%s" % i, hostname):
+            _conf = {
+                "classes":
+                [KEYS[i]]
+            }
+            print yaml.dump(_conf, explicit_start=True, \
+                default_flow_style=False)
+            return
+
+    # 从 sys 上获取节点来配置.
     _ret = get_classes_to_nodes()
     if not _ret[0]:
         print _ret[1]
@@ -55,15 +102,14 @@ def main():
             _classes = classes
             break
 
-    # 设默认值.
-    if "_classes" not in locals():
-        _classes = "puppet_default" 
-
+    if "_classes" not in locals():   # 添加默认配置.
+        _classes = "puppet_default"
     _conf = {
         "classes":
         [_classes]
     }
-    print yaml.dump(_conf, explicit_start=True, default_flow_style=False)
+    print yaml.dump(_conf, explicit_start=True, \
+        default_flow_style=False)
     
 
 if __name__ == '__main__':

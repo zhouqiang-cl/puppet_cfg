4fa135211d56b78552048002ead130b87f0ef4b3
wateer <liningning@wandoujia.com>
2015-04-15 13:54:48 +0800

    add puppet_api.py

---
+++ tools/puppet_api.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import json
+import requests
+from requests import Request, Session
+
+
+dashboard_host = "puppetdashboard.corp.nosa.me"
+certificate_host = "puppetca1.nosa01:8140"
+
+
+def hosts_failed():
+    """ 查找哪些机器 Puppet 执行失败.
+
+    curl 命令格式如下:
+        curl -H "Accept: application/json" http://dashboard_host/nodes/failed
+
+    """
+    url = "http://%s/nodes/failed" % dashboard_host
+    r = requests.get(url, headers={"Accept":"application/json"})
+
+    return [ i["name"] for i in json.loads(r.text) ]
+
+
+def certificate_get(hostname):
+    """ 查看给定机器的证书信息.
+
+    curl 命令格式如下:
+        curl -k -H 'Accept: pson' https://certificate_host/production/certificate_status/hostname
+
+    """
+    url = "https://%s/production/certificate_status/%s" % (certificate_host, hostname)
+    s = Session()
+    req = Request('GET', url,
+        headers={"Accept": "pson"}
+    )
+    prepped = req.prepare()
+    r = s.send(prepped,
+        verify=False,
+    )
+    return r.status_code, r.text
+
+
+def certificate_delete(hostname):
+    """ 删除给定机器的证书.
+
+    curl 命令格式如下:
+        curl -k -X DELETE -H 'Accept: pson' https://certificate_host/production/certificate_status/hostname
+
+    """
+    url = "https://%s/production/certificate_status/%s" % (certificate_host, hostname)
+    s = Session()
+    req = Request('DELETE', url,
+        headers={"Accept": "pson"}
+    )
+    prepped = req.prepare()
+    r = s.send(prepped,
+        verify=False,
+    )
+    return r.status_code, r.text
+
+
+if __name__ == "__main__":
+     hostname = "pxe0.hy01.nosa.me"
+     print certificate_get(hostname)
+     print certificate_delete(hostname)
+     print certificate_get(hostname)
